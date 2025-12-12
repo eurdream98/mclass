5aeee1b7abe1c1b3ac583a8ac8a8f86d39955c43
eurdream98 <k01054792361@gmail.com>
2025-12-12 14:25:39 +0900

    feat: 회원 목록 페이지 추가

---
+++ src/main/java/com/example/demo/Member.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.persistence.Column;
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Member{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false,length=50)
+    private String name;
+
+    @Column(unique=true,nullable=false,length=100)
+    private String email;
+}
